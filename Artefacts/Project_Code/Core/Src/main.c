be3872e4470662e7883015919b07aa3fc17e1b8f
Abdul Rafey <abdul.rafey476@hotmail.com>
2026-05-10 00:35:51 +0500

    Add AI confidence fusion and UART decision output

--- Artefacts/Project_Code/Core/Src/main.c
+++ Artefacts/Project_Code/Core/Src/main.c
@@ -43,44 +43,92 @@ int main(void)
     ModelManager_Init();
 
     char start[] = "STM32 USART2 START\r\n";
-    HAL_UART_Transmit(&huart2, (uint8_t *)start, strlen(start), HAL_MAX_DELAY);
+    HAL_UART_Transmit(&huart2, (uint8_t*)start, strlen(start), HAL_MAX_DELAY);
 
     int ai_ready = 0;
-    if (ModelManager_TFLM_Init())
-    {
+    if (ModelManager_TFLM_Init()) {
         ai_ready = 1;
         char ok[] = "AI INIT OK\r\n";
-        HAL_UART_Transmit(&huart2, (uint8_t *)ok, strlen(ok), HAL_MAX_DELAY);
-    }
-    else
-    {
+        HAL_UART_Transmit(&huart2, (uint8_t*)ok, strlen(ok), HAL_MAX_DELAY);
+    } else {
         char fail[] = "AI INIT FAILED\r\n";
-        HAL_UART_Transmit(&huart2, (uint8_t *)fail, strlen(fail), HAL_MAX_DELAY);
+        HAL_UART_Transmit(&huart2, (uint8_t*)fail, strlen(fail), HAL_MAX_DELAY);
     }
     /* USER CODE END 2 */
 
     /* USER CODE BEGIN WHILE */
     while (1)
     {
-        char msg[180];
+        char msg[260];
 
         HardwareProfiler_Sample();
         HardwareState hw = HardwareProfiler_GetState();
 
         ModelVariant rule_selected = AdaptationEngine_Decide(hw);
-        const char *rule_model = AdaptationEngine_ToString(rule_selected);
+        const char*  rule_model    = AdaptationEngine_ToString(rule_selected);
 
-        sprintf(msg,
-                "[HW] T=%.2fC | V=%.2fV | CPU=%.2f%% | Rule=%s\r\n",
+        if (ai_ready) {
+            int ok = ModelManager_RunInference(
                 hw.temperature_c,
-                hw.vcc_volts,
-                hw.cpu_load_percent,
-                rule_model);
+                hw.cpu_load_percent);
 
-        HAL_UART_Transmit(&huart2, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+            if (ok) {
+                float o0 = ModelManager_GetOutput(0);
+                float o1 = ModelManager_GetOutput(1);
+                float o2 = ModelManager_GetOutput(2);
+
+                int   pred = 0;
+                float conf = o0;
+                if (o1 > conf) { conf = o1; pred = 1; }
+                if (o2 > conf) { conf = o2; pred = 2; }
+
+                const char* ai_model =
+                    (pred == 0) ? "STANDARD_INT8"  :
+                    (pred == 1) ? "PRUNED_VARIANT"  :
+                                  "MIXED_PRECISION";
+
+                ModelVariant final_decision;
+                const char*  decision_src;
+
+                if (conf >= 0.75f) {
+                    final_decision = (ModelVariant)pred;
+                    decision_src   = "AI";
+                } else {
+                    final_decision = rule_selected;
+                    decision_src   = "RULE";
+                }
+
+//                const char* conf_label =
+//                    (conf > 0.85f) ? "High"   :
+//                    (conf > 0.60f) ? "Medium" : "Low";
+
+                sprintf(msg,
+                    "[HW] T=%.2fC | V=%.2fV | CPU=%.2f%% | "
+                    "AI=%s(%.3f) | Rule=%s | Decided=%s | Src=%s\r\n",
+                    hw.temperature_c, hw.vcc_volts, hw.cpu_load_percent,
+                    ai_model, conf,
+                    rule_model,
+                    AdaptationEngine_ToString(final_decision),
+                    decision_src);
+            } else {
+                sprintf(msg,
+                    "[HW] T=%.2fC | V=%.2fV | CPU=%.2f%% | "
+                    "Rule=%s | AI=FAILED\r\n",
+                    hw.temperature_c, hw.vcc_volts, hw.cpu_load_percent,
+                    rule_model);
+            }
+        } else {
+            sprintf(msg,
+                "[HW] T=%.2fC | V=%.2fV | CPU=%.2f%% | "
+                "Rule=%s | AI=NOT_READY\r\n",
+                hw.temperature_c, hw.vcc_volts, hw.cpu_load_percent,
+                rule_model);
+        }
+
+        HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
         HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
         HAL_Delay(2000);
-    } 
+    }
     /* USER CODE END WHILE */
 }
 
@@ -91,18 +139,18 @@ void SystemClock_Config(void)
     __HAL_RCC_PWR_CLK_ENABLE();
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 8;
-    RCC_OscInitStruct.PLL.PLLN = 336;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 7;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-        Error_Handler();
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM       = 8;
+    RCC_OscInitStruct.PLL.PLLN       = 336;
+    RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ       = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) Error_Handler();
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                     | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
@@ -112,87 +160,81 @@ void SystemClock_Config(void)
 static void MX_ADC1_Init(void)
 {
     ADC_ChannelConfTypeDef sConfig = {0};
-    hadc1.Instance = ADC1;
-    hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
-    hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-    hadc1.Init.ScanConvMode = DISABLE;
-    hadc1.Init.ContinuousConvMode = DISABLE;
+    hadc1.Instance                   = ADC1;
+    hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
+    hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
+    hadc1.Init.ScanConvMode          = DISABLE;
+    hadc1.Init.ContinuousConvMode    = DISABLE;
     hadc1.Init.DiscontinuousConvMode = DISABLE;
-    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    hadc1.Init.NbrOfConversion = 1;
+    hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
+    hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion       = 1;
     hadc1.Init.DMAContinuousRequests = DISABLE;
-    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-    if (HAL_ADC_Init(&hadc1) != HAL_OK)
-        Error_Handler();
-    sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
-    sConfig.Rank = 1;
+    hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+    if (HAL_ADC_Init(&hadc1) != HAL_OK) Error_Handler();
+    sConfig.Channel      = ADC_CHANNEL_TEMPSENSOR;
+    sConfig.Rank         = 1;
     sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-        Error_Handler();
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) Error_Handler();
 }
 
 static void MX_I2C1_Init(void)
 {
-    hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 100000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c1.Init.OwnAddress1 = 0;
-    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Instance             = I2C1;
+    hi2c1.Init.ClockSpeed      = 100000;
+    hi2c1.Init.DutyCycle       = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1     = 0;
+    hi2c1.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.OwnAddress2     = 0;
     hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-        Error_Handler();
+    hi2c1.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK) Error_Handler();
 }
 
 static void MX_I2S3_Init(void)
 {
-    hi2s3.Instance = SPI3;
-    hi2s3.Init.Mode = I2S_MODE_MASTER_TX;
-    hi2s3.Init.Standard = I2S_STANDARD_PHILIPS;
-    hi2s3.Init.DataFormat = I2S_DATAFORMAT_16B;
-    hi2s3.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
-    hi2s3.Init.AudioFreq = I2S_AUDIOFREQ_96K;
-    hi2s3.Init.CPOL = I2S_CPOL_LOW;
-    hi2s3.Init.ClockSource = I2S_CLOCK_PLL;
+    hi2s3.Instance            = SPI3;
+    hi2s3.Init.Mode           = I2S_MODE_MASTER_TX;
+    hi2s3.Init.Standard       = I2S_STANDARD_PHILIPS;
+    hi2s3.Init.DataFormat     = I2S_DATAFORMAT_16B;
+    hi2s3.Init.MCLKOutput     = I2S_MCLKOUTPUT_ENABLE;
+    hi2s3.Init.AudioFreq      = I2S_AUDIOFREQ_96K;
+    hi2s3.Init.CPOL           = I2S_CPOL_LOW;
+    hi2s3.Init.ClockSource    = I2S_CLOCK_PLL;
     hi2s3.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
-    if (HAL_I2S_Init(&hi2s3) != HAL_OK)
-        Error_Handler();
+    if (HAL_I2S_Init(&hi2s3) != HAL_OK) Error_Handler();
 }
 
 static void MX_SPI1_Init(void)
 {
-    hspi1.Instance = SPI1;
-    hspi1.Init.Mode = SPI_MODE_MASTER;
-    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-    hspi1.Init.NSS = SPI_NSS_SOFT;
+    hspi1.Instance               = SPI1;
+    hspi1.Init.Mode              = SPI_MODE_MASTER;
+    hspi1.Init.Direction         = SPI_DIRECTION_2LINES;
+    hspi1.Init.DataSize          = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity       = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase          = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS               = SPI_NSS_SOFT;
     hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    hspi1.Init.CRCPolynomial = 10;
-    if (HAL_SPI_Init(&hspi1) != HAL_OK)
-        Error_Handler();
+    hspi1.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode            = SPI_TIMODE_DISABLE;
+    hspi1.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
+    hspi1.Init.CRCPolynomial     = 10;
+    if (HAL_SPI_Init(&hspi1) != HAL_OK) Error_Handler();
 }
 
 static void MX_USART2_UART_Init(void)
 {
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Instance          = USART2;
+    huart2.Init.BaudRate     = 115200;
+    huart2.Init.WordLength   = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits     = UART_STOPBITS_1;
+    huart2.Init.Parity       = UART_PARITY_NONE;
+    huart2.Init.Mode         = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
     huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart2) != HAL_OK)
-        Error_Handler();
+    if (HAL_UART_Init(&huart2) != HAL_OK) Error_Handler();
 }
 
 static void MX_GPIO_Init(void)
@@ -207,10 +249,10 @@ static void MX_GPIO_Init(void)
 
     HAL_GPIO_WritePin(CS_I2C_SPI_GPIO_Port, CS_I2C_SPI_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOD, LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin|Audio_RST_Pin, GPIO_PIN_RESET);
 
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 
     GPIO_InitStruct.Pin = CS_I2C_SPI_Pin;
@@ -219,61 +261,53 @@ static void MX_GPIO_Init(void)
     GPIO_InitStruct.Pin = OTG_FS_PowerSwitchOn_Pin;
     HAL_GPIO_Init(OTG_FS_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = PDM_OUT_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pin       = PDM_OUT_Pin;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
     HAL_GPIO_Init(PDM_OUT_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = B1_Pin;
+    GPIO_InitStruct.Pin  = B1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = BOOT1_Pin;
+    GPIO_InitStruct.Pin  = BOOT1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     HAL_GPIO_Init(BOOT1_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = CLK_IN_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pin       = CLK_IN_Pin;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
     HAL_GPIO_Init(CLK_IN_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pin   = LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin|Audio_RST_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = OTG_FS_ID_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pin       = OTG_FS_ID_Pin;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
     HAL_GPIO_Init(OTG_FS_ID_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pin       = GPIO_PIN_11|GPIO_PIN_12;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = OTG_FS_OverCurrent_Pin;
+    GPIO_InitStruct.Pin  = OTG_FS_OverCurrent_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(OTG_FS_OverCurrent_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = MEMS_INT2_Pin;
+    GPIO_InitStruct.Pin  = MEMS_INT2_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
     HAL_GPIO_Init(MEMS_INT2_GPIO_Port, &GPIO_InitStruct);
 }
 
-void Error_Handler(void)
-{
-    while (1)
-        ;
-}
+void Error_Handler(void) { while(1); }
 
 #ifdef USE_FULL_ASSERT
-void assert_failed(uint8_t *file, uint32_t line)
-{
-    (void)file;
-    (void)line;
-}
+void assert_failed(uint8_t *file, uint32_t line) { (void)file; (void)line; }
 #endif
